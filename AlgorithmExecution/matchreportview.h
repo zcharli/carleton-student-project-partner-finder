c7e2498689f0a79597e9c2f1949ad0bc0adaf01f
Charlie <gogogeorge123@yahoo.ca>
2015-11-29 22:15:16 -0500

    created template UI

--- AlgorithmExecution/matchreportview.h
+++ AlgorithmExecution/matchreportview.h
@@ -1,10 +1,22 @@
 #ifndef MATCHREPORTVIEW_H
 #define MATCHREPORTVIEW_H
 
-class MatchReportView
+#include <QWidget>
+
+namespace Ui {
+class MatchReportView;
+}
+
+class MatchReportView : public QWidget
 {
+    Q_OBJECT
+
 public:
-    MatchReportView();
+    explicit MatchReportView(QWidget *parent = 0);
+    ~MatchReportView();
+
+private:
+    Ui::MatchReportView *ui;
 };
 
 #endif // MATCHREPORTVIEW_H
